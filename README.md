e3bb3e8ca69b57a2bdb6eae52c330209f262857c
natromitus <sebastian.stupak@maethril.net>
2023-12-28 22:40:17 +0100

    AllVsTrueForAll benchmark, readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # linq-problems
 Showcase of common LINQ problems
+
+## LINQ All vs List TrueForAll
+
+```
+BenchmarkDotNet v0.13.11, Windows 10 (10.0.19045.3803/22H2/2022Update)
+AMD Ryzen 5 5600U with Radeon Graphics, 1 CPU, 12 logical and 6 physical cores
+.NET SDK 8.0.100-rc.2.23502.2
+  [Host]     : .NET 8.0.0 (8.0.23.47906), X64 RyuJIT AVX2
+  DefaultJob : .NET 8.0.0 (8.0.23.47906), X64 RyuJIT AVX2
+
+| Method           | Mean      | Error     | StdDev    | Median    | Allocated |
+|----------------- |----------:|----------:|----------:|----------:|----------:|
+| All              | 13.142 us | 0.0644 us | 0.0503 us | 13.135 us |      40 B |
+| TrueForAll       |  7.295 us | 0.0100 us | 0.0088 us |  7.293 us |         - |
+| ToListTrueForAll | 10.239 us | 0.2632 us | 0.7719 us |  9.906 us |   40056 B |
+```
---
+++ src/LinqProblems/AllVsTrueForAll/Benchmarks.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace LinqProblems.AllVsTrueForAll;
+
+[MemoryDiagnoser(false)]
+public class Benchmarks
+{
+    private const int SampleSize = 10_000;
+    private List<int>? _arItems;
+    private IEnumerable<int> EnumerableItems => _arItems!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        Random rnd = new(42);
+        _arItems = Enumerable
+            .Range(0, SampleSize)
+            .Select(_ => rnd.Next(1, int.MaxValue))
+            .ToList();
+    }
+
+    [Benchmark]
+    public bool All()
+    {
+        return EnumerableItems.All(x => x > 0);
+    }
+
+    [Benchmark]
+    public bool TrueForAll()
+    {
+        return _arItems!.TrueForAll(x => x > 0);
+    }
+
+    [Benchmark]
+    public bool ToListTrueForAll()
+    {
+        return EnumerableItems.ToList().TrueForAll(x => x > 0);
+    }
+}
